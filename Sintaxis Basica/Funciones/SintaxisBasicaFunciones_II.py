5d1bc07eb1b7d77544113fa3a3100c8f1460c3e2
DrohanxX <johancacerez5@gmail.com>
2022-11-07 17:54:45 -0600

    Funciones II
    Se explica como se usan las funciones con y sin parametros
    
    La utilidad de pasar parametros
    
    el metodo return

---
+++ Sintaxis Basica/Funciones/SintaxisBasicaFunciones_II.py
@@ -0,0 +1,20 @@
+#Funciones
+
+#Paso de parametros
+
+def sumar(num1, num2):
+
+    resultado = num1 + num2
+
+    return resultado
+
+
+print(sumar(5,7))
+
+almacena_resultado = sumar(5,8)
+
+print(almacena_resultado)
+
+#Esta ukltima no se ve ya que no muestra ningun resultado
+sumar(35,358)
+
