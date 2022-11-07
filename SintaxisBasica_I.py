1ecf8c5c311b1e0d56d59e77b15a5b86a853a392
DrohanxX <johancacerez5@gmail.com>
2022-11-07 16:26:38 -0600

    Sintaxis basica I
    Se introduce como es la sintaxis basica al momento de escribir en Python

---
+++ SintaxisBasica_I.py
@@ -0,0 +1,20 @@
+#Primeras funciones de python
+
+
+print("Hola mundo")
+#No ternina con ";" como en los lenguajes de programacion
+
+print ("Hola mundo"); print("Adios mundo")
+#Se pueden dar mas instrucciones en una sola linea usando ";"
+#No es recomendable hacerlo
+
+#Para hacer un salto de linea se usa "\"
+mi_nombre = "Hola mi nombre es \
+    Johan"
+
+#Se hace el uso de la identacion para el codigo
+a = 0
+for i in range(5):
+    a += 1
+    print(a)
+#Identacion
