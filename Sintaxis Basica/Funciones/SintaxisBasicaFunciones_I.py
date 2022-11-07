cb863ea228102c820dddb96d16bc6b7f1752d526
DrohanxX <johancacerez5@gmail.com>
2022-11-07 17:30:30 -0600

    Sintaxis Basica Funciones I
    Se explica que son las funciones
    Cual es su utilidad
    Cual es su sintaxis
    Como se ejecuta

---
+++ Sintaxis Basica/Funciones/SintaxisBasicaFunciones_I.py
@@ -0,0 +1,35 @@
+#Que son las funciones
+""""
+Conjunto de lineas de codigo agrupadas (bloque de codigo) que funcionan
+como una unidad realizando una tarea especifica.
+
+Las funciones en Python pueden devolver valores.
+
+Las funciones en Python pueden tener parametros/argumentos.
+
+A las funciones tambien se les conoce como "metodos" cuando se encuentran
+definidas dentro de una clase.
+"""
+#Utilidad
+"""
+Reutilizacion de codigo 
+"""
+#Sintaxis
+"""
+def nombre_funcion(parametros/argumentos):
+
+    instrucciones de la funcion
+
+    return (opcional)
+"""
+#Ejecucion
+"""
+nombre_funcion()
+
+nombre_funcion(parametros)
+"""
+def mensaje():  #Declaracion
+    print("Estamos aprendiendo python")
+    print("Estamos apredniendo instrucciones basicas")  #Cuerpo de la funcion
+
+mensaje()   #Llamada a la funcion
