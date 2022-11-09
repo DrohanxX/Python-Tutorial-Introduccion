00c96bcd3f976bd2c1aabfc0ac2ed8a1067d188c
DrohanxX <johancacerez5@gmail.com>
2022-11-09 17:27:26 -0600

    Bucles I
    Se da una introduccion a los bucles en python

---
+++ Bucles/Bucles_I.py
@@ -0,0 +1,53 @@
+#Bucles
+
+#Tipos
+
+#Hay dos tipos de bucles
+
+    #Determinados
+
+        #Se ejecuta un numero determinado de veces
+
+        #Se aabre a priori cuantas veces se va a ejecutar el codigo del interior del bucle
+
+    #Indeterminados
+
+        #Se ejecuta un numero indeterminado de veces
+
+        #No se sabe a priori cuantas veces se va a ejecutar el codigo del interior del bucle
+
+        #El numero de veces que se ejecutara depednera de las circunstancias durante la ejecucion del programa
+
+#Bucle
+
+"""
+Instruccion
+Instruccion
+Instruccion
+
+Declaracion del bucle:
+
+    Cuerpo del bucle
+
+Instruccion
+Instruccion
+Instruccion
+"""
+
+#Bucle for (determinado) Sintaxis
+
+#Elemento a recorrer: puede ser lista, tupla, cadena de texto, rango
+
+#   for variable in elemento a recorrer:
+
+    #   Cuerpo del bucle
+
+
+for i in [1,2,3]:
+
+    print("Hola")
+
+
+for estaciones in ["Primavera","Verano","Otoño","Invierno"]:
+
+    print(estaciones)
