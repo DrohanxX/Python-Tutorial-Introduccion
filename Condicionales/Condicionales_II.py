2fb6e87728133fb9c2593764d5b3bc6b2378a71e
DrohanxX <johancacerez5@gmail.com>
2022-11-09 16:13:22 -0600

    Condicionales II
    Se explcia el uso de:
    if, else, elif

---
+++ Condicionales/Condicionales_II.py
@@ -0,0 +1,19 @@
+#Condicionales
+
+#Intruccion if, else, elif
+
+print("Verificacion de acceso")
+
+edad_usuario = int(input("Introduce tu edad: "))
+
+if edad_usuario < 18:
+
+    print("No puedes pasar")
+
+elif edad_usuario > 100:
+
+    print("Edad incorrecta")
+
+else:
+
+    print("Puedes pasar")
