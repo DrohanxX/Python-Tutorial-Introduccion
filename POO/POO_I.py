40e777a94f31a67f3f8cfaf253c5f8a594c4fee3
DrohanxX <johancacerez5@gmail.com>
2022-11-14 11:15:43 -0600

    POO
    Introducion a POO (Programacion Orientada a Objetos)

---
+++ POO/POO_I.py
@@ -0,0 +1,65 @@
+#Programacion Orientada a Objetos
+
+
+#   Paradigmas de la programacion
+
+
+#       Programacion orientada a procedimientos
+
+#           Lenguajes: Fortran, Cobol, Basic, etc
+
+#           Desventajas:
+#               Unidades de codigo muy grandes en aplicaciones objetas
+#               En aplicaciones complejas el codigo resulta dificil de desifrar
+#               Poco reutilizable
+#               Si existe fallo en alguna linea del codigo, es muy probable que el programa caiga
+#               Aparicion frecuente de codigo espagueti
+#               Dificil de depurar por otros programadores en caso de necesidad o error
+
+
+#       Programacion orientada a objetos
+
+#           Lenguajes: C++, Javaj, Visual.NET, etc
+
+#           Ventajas:
+#               Programas divididos en trozos, partes, modulos o clases. Modularizacion
+#               Muy reutilizable. Herencia
+#               Si existe un fallo en alguna linea de codigo, el programa continuara. Tratamiento de excepciones
+#               Encapsulamiento
+
+#           Vocabulario:
+#               Clase
+#               Objeto
+#               Ejemplar clase. Instancia de clase. Ejemplarizar una clase. Instanciar una clase
+#               Modularizacion
+#               Encapsulamiento
+#               Herencia
+#               Polimorfismo
+
+#           En que consiste
+#               Transladar la naturaleza de los objetos de la vida real al codigo de programacion
+
+#           Cual es la naturaleza de un objeto de la vida real
+#               Los objetos tienen un estado, un comportamiento(¿que puede hacer?) y unas propiedades
+
+#           El objeto coche
+#           Cual es el estado de un coche? parado, circulando, aparcado, etc
+#           Que propiedades tiene un coche? tiene un color, un peso, un tamaño
+#           Que coomportamiento tiene un coche? puede arrancar, frenar, acelerar, girar
+
+
+#           Objeto
+
+#           Tiene propiedades (atributos)
+#               color
+#               peso
+#               alto
+#               largo
+
+#           Tiene un comportamiento (que es capaz de hacer)
+#               arrancar
+#               frenar
+#               girar
+#               Acelerar
+
+
