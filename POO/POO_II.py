621df1bc9e30aec915b6e8deb8aa8051a2e2c05c
DrohanxX <johancacerez5@gmail.com>
2022-11-14 11:37:04 -0600

    POO II
    Vocabulario de POO
    y pseudocodigo

---
+++ POO/POO_II.py
@@ -0,0 +1,56 @@
+#POO
+
+#   Conceptos o terminos de la POO
+
+#           Vocabulario:
+#               Clase
+#               Objeto
+#               Ejemplar clase. Instancia de clase. Ejemplarizar una clase. Instanciar una clase
+#               Modularizacion
+#               Encapsulamiento
+#               Herencia
+#               Polimorfismo
+
+#   Clase
+
+#       Modelo donde se redactan las "caracteristicas comunes" de un grupo de objetos
+
+#           Chasis comun
+#           4 Ruedas
+
+
+#   Instancia, Ejemplar, objeto de clase
+
+#       Ejemplar perteneciente a una clase
+
+
+#   Modularizacion
+
+#       Clase 1
+#       Clase 2
+#       Clase 3
+
+
+#   Encapsulacion
+
+#       Se conectan a travez de "Metodos de acceso" para conetar entre clases
+
+
+#   Objeto
+
+#       Accesdiendo a propiedades y comportamiento (pseudocodigo)
+#       Nomenclatura del punto
+
+#           Objeto (miCoche)
+
+#               Accediendo a propiedades de objeto desde codigo (pseudocodigo)
+#                   miCoche.color = "rojo";
+#                   miCoche.peso = 1500;
+#                   miCoche.ancho = 2000;
+#                   miCoche.alto = 900;
+
+#               Accediendo a comportamiento de objeto desde codigo (pseudocodigo)
+#                   miCoche.arrancar();
+#                   miCoche.frena();
+#                   miCoche.gira();
+#                   miCoche.acelera();
