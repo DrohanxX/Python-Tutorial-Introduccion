dd6f94d6168f1daee4068e7b72f7cd8aaaf2f9f8
DrohanxX <johancacerez5@gmail.com>
2022-11-21 12:28:53 -0600

    Paquetes
    Como se crean paquetes para ser usados desde otros archivos aun que estos se encuentren en carpetas diferentes

---
+++ Paquetes/Paquetes.py
@@ -0,0 +1,21 @@
+#Paquetes 
+
+#      Que son
+
+#           Directorios donde se almacenan modulos relacionados entre si
+
+#       Para que sirven
+
+#           Para organizar y reutilizar los modulos
+
+#       Como se crea un paquete
+
+#           Crear una carpeta con un archivo __init__.py
+
+#El paquete se encuentra en: Python/Paquetes/Calculos/Calculos_generales.py
+#El archivo que lama las funciones esta en: Python/Paquetes/Paquetes.py
+
+from Calculos.Calculos_generales import *
+
+sumar(4,6)
+
